1dcf3998dd5856428b8ee2478385ef01440dff89
Marcos Oliveira <90332041+devMarcos2021@users.noreply.github.com>
2022-05-07 09:44:13 -0300

    Atualização
    Adicionado novas funcionalidades ao site.

--- script.js
+++ script.js
@@ -4,6 +4,31 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+        }if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     });
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    var typed = new Typed(".typing", {
+        strings:["Desenvolvedor","Freelancer","Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings:["Desenvolvedor","Freelancer","Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+
+    })
 });
