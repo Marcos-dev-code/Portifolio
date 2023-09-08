a4d0163d9fbcda87109462a1fd797d66dafb4501
Your Name <you@example.com>
2023-09-08 17:45:54 -0300

    otimização do código
    mudança para melhor compreensão do código.

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+$(document).ready(function () {
+    // Função para adicionar ou remover a classe "sticky" na barra de navegação
+    function toggleStickyNavbar() {
+        $('.navbar').toggleClass("sticky", $(window).scrollTop() > 20);
+    }
+
+    // Função para mostrar ou ocultar o botão de rolagem para o topo
+    function toggleScrollUpButton() {
+        $('.scroll-up-btn').toggleClass("show", $(window).scrollTop() > 500);
+    }
+
+    // Função para manipular o clique no botão de rolagem para o topo
+    $('.scroll-up-btn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000);
     });
 
-    var typed = new Typed(".typing", {
-        strings:["Desenvolvedor","Freelancer","Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
+    // Função para manipular o clique nos itens do menu
+    $('.navbar .menu a').click(function (e) {
+        e.preventDefault(); // Evitar o comportamento padrão do link
+        const target = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(target).offset().top }, 300, function () {
+            // Após a rolagem, esconder o menu se estiver aberto
+            if ($('.navbar .menu').hasClass('active')) {
+                $('.navbar .menu').removeClass('active');
+                $('.menu-btn i').removeClass('active');
+            }
+        });
     });
-    var typed = new Typed(".typing-2", {
-        strings:["Desenvolvedor","Freelancer","Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    $('.menu-btn').click(function(){
+
+    // Função para inicializar a animação de digitação
+    function initTypingAnimation(selector) {
+        new Typed(selector, {
+            strings: ["Desenvolvedor", "Freelancer", "Designer"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+    }
+
+    // Inicializar a animação de digitação em elementos com as classes "typing" e "typing-2"
+    initTypingAnimation(".typing");
+    initTypingAnimation(".typing-2");
+
+    // Manipular o clique no botão do menu
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
 
-    })
+    // Monitorar o evento de rolagem da janela para atualizar a barra de navegação e o botão de rolagem para o topo
+    $(window).scroll(function () {
+        toggleStickyNavbar();
+        toggleScrollUpButton();
+    });
+
+    // Chame as funções para definir o estado inicial da barra de navegação e do botão de rolagem para o topo
+    toggleStickyNavbar();
+    toggleScrollUpButton();
 });
